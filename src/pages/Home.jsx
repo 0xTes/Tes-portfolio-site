f9d9aee7d0bca449cc03b81a23d80b5b00a59800
0xTes <tesdistro@gmail.com>
2026-06-30 10:22:06 +0100

    Home and component architecture

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Services from "../components/Services";
+import Work from "../components/Work";
+import LetsTalk from "../components/LetsTalk";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Services />
+      <Work />
+      <LetsTalk />
+    </>
+  );
+}
